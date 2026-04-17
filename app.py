1375e18b5d15ad264169b86604b5595ada6ed255
Anurag <prajapatianuradha011@gmail.com>
2026-04-17 17:25:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ def handle_message(data):
     }, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, use_reloader=False, port=5000)
+    socketio.run(app, debug=True, port=5001)
 
 send({
     "username": session.get('user'),
