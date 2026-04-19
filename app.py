b177bf1ddb335f42a5dffb051994f2a90e5992fa
Anurag <prajapatianuradha011@gmail.com>
2026-04-19 20:47:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-import eventlet
-eventlet.monkey_patch()
-from flask import Flask
+
 
 from flask import Flask, render_template, request, redirect, session
 from flask_socketio import SocketIO, send, emit
